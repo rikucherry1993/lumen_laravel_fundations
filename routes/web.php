65c77a1ef846a9cdd07e1d7e7ced2176e6976906
rikucherry1993 <rikucherry0522@gmail.com>
2021-09-20 15:51:57 +0900

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// uri: / means the root 
+// action: a closure = anonymous function. define/run at same context 
+// view(view name): a laravel helper function, to rend a desinated view.
+// view name: can be found at [resources/views]
+Route::get('/', function () {
+  return view('welcome');
+});
